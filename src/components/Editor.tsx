ac9c584f3fffb264ee23ccb369ed6014b883b79a
huazhouwang <huazhou19@gmail.com>
2021-08-11 12:25:51 +0800

    Fix Editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,28 +1,9 @@
 import AceEditor from "react-ace";
 import { IAceEditorProps } from "react-ace/lib/ace";
-import { useEffect } from "react";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-tomorrow_night_eighties";
 
 const Editor = (props: IAceEditorProps) => {
-  const { theme, mode } = props;
-
-  useEffect(() => {
-    try {
-      if (theme) {
-        require(`ace-builds/src-noconflict/theme-${theme}`);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-
-    try {
-      if (mode) {
-        require(`ace-builds/src-noconflict/mode-${mode}`);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }, [theme, mode]);
-
   return (
     <AceEditor
       width={"100%"}
